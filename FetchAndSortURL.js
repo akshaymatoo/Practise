62b6fc301f7cfe9aad0e0199a7521425c4ea3c2b
akshaymattoo <amattoo@us.ibm.com>
2019-02-05 17:10:09 -0800

    changed File with async awit

--- FetchAndSortURL.js
+++ FetchAndSortURL.js
@@ -1,60 +1,57 @@
-var http = require("https");
-var title = [];
-var total_pages = 0;
-var page = 1;
-var total = 0;
- let url = "https://jsonmock.hackerrank.com/api/movies/search/?Title=subs";
- 
+const request = require("request");
+let title = [];
+let total_pages = 0;
+let page_num = 1;
 
 function getMovieTitles (subsStr){
+  let url = "https://jsonmock.hackerrank.com/api/movies/search/?Title=subs&page="+page_num;
   if(subsStr)
-  url = url.replace("subs",subsStr);
-var req = http.get(url, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    var res = JSON.parse(body.toString());
-
-  
-  if(res.data.length>0 ){
-    total = res.total;
-    total_pages = res.total_pages;
-    page = parseInt(res.page);
-    page = page+1;
-    //console.log(total_pages,page);
+    url = url.replace("subs",subsStr);
     
-    res.data.forEach(function(value){
-      //console.log(value.Title)
-      title.push(value.Title);
-    })
-  
-    url =  url +"&page="+page;
-    //console.log(title)
-    if(page<=total_pages)
-      getMovieTitles();
-    else
-    {
-      if(total === title.length && title.length>0)
-        console.log (title.sort().join("\n"));
-      else{
-        console.log("No result!!")
-      }
-    }
-  }else{
-        console.log("No result!!")
-      }
-  
-    
-  });
-});
+  //console.log(url)
  
-req.end();
+ return new Promise((resolve, reject) => {
+    request (url,function (error,response,body){
+      if (error) reject(error);
+      
 
+      var movie_data = JSON.parse(body);
+
+       
+      total_pages = movie_data.total_pages;
+
+      
+      if(movie_data.data.length>0 && page_num<=total_pages){
+        
+        for(let movie of movie_data.data){
+          //console.log(movie)
+          title.push(movie.Title);
+        }
+        //console.log(title)
+        page_num++;
+        resolve(getMovieTitles(subsStr));
+      }
+      else{
+          resolve(title.sort().join("\n"));
+      }
+    });
+ });
+ 
+   
+  }
+ 
+async function getResult(){
+  let title_spiderman = await getMovieTitles("spiderman");
+  console.log(title_spiderman);
+  page_num = 1;
+  title = [];
+  console.log("-------------------");
+   let superman = await getMovieTitles("superman");
+  console.log(superman);
 }
 
- getMovieTitles("brother");
+getResult();
+ 
+
+
+ 
