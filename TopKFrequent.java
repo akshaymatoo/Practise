6b2bd64f8478051df880a49ac54980c7162d28ae
akshaymattoo <amattoo@us.ibm.com>
2019-02-12 14:16:33 -0800

    updated TopKFrequent.java

--- TopKFrequent.java
+++ TopKFrequent.java
@@ -7,7 +7,32 @@ public class Main{
       System.out.println(lst);
     }
 
+
  public List<String> topKFrequent(String[] words, int k) {
+        
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        //builds the hashmap
+        for(String word:words)
+            map.put(word,map.getOrDefault(word,0)+1);
+        
+        //Now build a priority queue
+        PriorityQueue<String> heap = new PriorityQueue<String>(
+            (w1,w2) -> map.get(w1).equals(map.get(w2))?w1.compareTo(w2):map.get(w2)-map.get(w1)
+        );
+        
+        //its a max heap 
+         for (String word: map.keySet()) 
+             heap.offer(word);
+        
+
+        List<String> lst = new ArrayList<String>();
+        while(!heap.isEmpty())
+          lst.add(heap.poll());
+        
+        
+        return lst.subList(0,k);
+    }
+ public List<String> topKFrequent2(String[] words, int k) {
         Map<String, Integer> count = new HashMap();
         for (String word: words) {
             count.put(word, count.getOrDefault(word, 0) + 1);
@@ -37,9 +62,11 @@ System.out.println("---After---"+heap);
             count.put(word, count.getOrDefault(word, 0) + 1);
         }
         List<String> candidates = new ArrayList(count.keySet());
+        //This sorts the list based on values in the hash map
         Collections.sort(candidates, (w1, w2) -> count.get(w1).equals(count.get(w2)) ?
                 w1.compareTo(w2) : count.get(w2) - count.get(w1));
         
+        //cuts the list for the given number of k
         return candidates.subList(0, k);
    }
 }
